21981e87e8a7ea8d6fbde21d60ad55f01f130705
Tolux001 <toluwalasepeter100@gmail.com>
2024-05-13 00:36:56 +0100

    Upload code

---
+++ app.js
@@ -0,0 +1,56 @@
+const items = [
+		{
+			id: 1,
+			title: "buttermilk pancakes",
+			img: "./images/item-1.jpeg",
+			link: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+		},
+		{
+			id: 2,
+			title: "diner double",
+			img: "./images/item-2.jpeg",
+			link: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+		},
+		{
+			id: 3,
+			title: "godzilla milkshake",
+			img: "./images/item-3.jpeg",
+			link: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+		},
+		{
+			id: 4,
+			title: "country delight",
+			img: "./images/item-4.jpeg",
+			link: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+		},
+		{
+			id: 5,
+			title: "egg attack",
+			img: "./images/item-5.jpeg",
+			link: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+		},
+		{
+			id: 6,
+			title: "oreo dream",
+			img: "./images/item-6.jpeg",
+			link: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+		},
+		{
+			id: 7,
+			title: "bacon overflow",
+			img: "./images/item-7.jpeg",
+			link: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+		},
+		{
+			id: 8,
+			title: "american classic",
+			img: "./images/item-8.jpeg",
+			link: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+		},
+		{
+			id: 9,
+			title: "quarantine buddy",
+			img: "./images/item-9.jpeg",
+			link: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb crucifix microdosing.`,
+		}
+]
